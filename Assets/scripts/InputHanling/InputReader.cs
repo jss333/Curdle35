47871b52efd12e56985688005e3f632e7b547e43
MostafaEbeed <en.mostfa4@gmail.com>
2025-02-08 17:36:39 +0200

    CameraController

---
+++ Assets/scripts/InputHanling/InputReader.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, InputActions.IGameplayActions
+{
+    #region Singleton
+
+    public static InputReader Singleton;
+    
+    private void Awake()
+    {
+        if (Singleton == null)
+            Singleton = this;
+        else
+        {
+            if (Singleton != this)
+                Destroy(gameObject);
+        }
+    }
+
+    #endregion
+    
+    InputActions inputActions;
+
+
+    public Action<bool> OnCameraPanning;
+    public Action<float> OnMouseZoom;
+    public Action<Vector2> OnUpdateMousePosition;
+    public Vector2 MouseDelta;
+    
+    
+    void Start()
+    {
+        inputActions = new InputActions();
+        
+        inputActions.Gameplay.SetCallbacks(this);
+        inputActions.Gameplay.Enable();
+    }
+
+    void Update()
+    {
+    }
+
+    public void OnCameraPan(InputAction.CallbackContext context)
+    {
+        OnCameraPanning?.Invoke(context.ReadValueAsButton());
+    }
+
+    public void OnCameraPanningVector(InputAction.CallbackContext context)
+    {
+        MouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMousePosition(InputAction.CallbackContext context)
+    {
+        OnUpdateMousePosition?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnCameraZoom(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnCameraZoomIn: {context.ReadValue<Vector2>()}");
+        OnMouseZoom?.Invoke(context.ReadValue<Vector2>().y);
+    }
+}
